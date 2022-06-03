96ea0b4b47df4feed39d4181d98affa89246940b
Golec1998 <56236713+Golec1998@users.noreply.github.com>
2022-06-04 00:17:45 +0200

    Added basic menu

---
+++ Sleep Tight/Assets/Scripts/MenuInput.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInput : MonoBehaviour
+{
+
+    public Animator menuCamController;
+    public LayerMask menuLayer;
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack"))
+            performClick();
+    }
+
+    private void performClick()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, menuLayer))
+        {
+            switch(hitInfo.collider.tag)
+            {
+                case "PlayButton":
+                    showPlay();
+                    break;
+                case "MonstersButton":
+                    showMonsters();
+                    break;
+                case "OptionsButton":
+                    showOptions();
+                    break;
+                case "ExitButton":
+                    showExit();
+                    break;
+                case "Stage1":
+                    startStage(1);
+                    break;
+            }
+            Debug.Log(hitInfo.collider.tag);
+        }
+    }
+
+    private void showPlay() {menuCamController.SetTrigger("Play");}
+
+    private void showMonsters() {menuCamController.SetTrigger("Monsters");}
+
+    private void showOptions() {menuCamController.SetTrigger("Options");}
+
+    private void showExit() {menuCamController.SetTrigger("Exit");}
+
+    private void startStage(int lvlNumber)
+    {
+        if(lvlNumber == 1)
+            SceneManager.LoadScene("TestWorld");
+    }
+
+}
