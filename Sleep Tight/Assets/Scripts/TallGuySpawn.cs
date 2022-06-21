31669e070980eb1f7653aa3222eebae66148600b
Golec1998 <56236713+Golec1998@users.noreply.github.com>
2022-06-22 01:27:38 +0200

    Added wardrobe

--- Sleep Tight/Assets/Scripts/TallGuySpawn.cs
+++ Sleep Tight/Assets/Scripts/TallGuySpawn.cs
@@ -8,12 +8,49 @@ public class TallGuySpawn : MonoBehaviour
     public GameObject TallGuy;
     public Transform[] path;
 
-    //void Start() { spawn(); }
+    public Transform doorL;
+    public Transform doorR;
+
+    float target = 100f;
+    Quaternion startAngle;
+    Quaternion targetAngleL;
+    Quaternion targetAngleR;
+    Quaternion newAngleL;
+    Quaternion newAngleR;
+
+    void Start()
+    {
+        startAngle.eulerAngles = new Vector3(-90, 180, 0);
+        targetAngleL.eulerAngles = new Vector3(-90, 180, target);
+        targetAngleR.eulerAngles = new Vector3(-90, 180, -target);
+        newAngleL = startAngle;
+        newAngleR = startAngle;
+    }
+
+    void Update()
+    {
+        doorL.rotation = Quaternion.Slerp(doorL.rotation, newAngleL, Time.deltaTime * 2f);
+        doorR.rotation = Quaternion.Slerp(doorR.rotation, newAngleR, Time.deltaTime * 1.7f);
+    }
 
     public void spawn()
     {
         GameObject tg = Instantiate(TallGuy, transform.position, transform.rotation);
         tg.GetComponent<TallGuyAI>().setPath(path);
+        /*
+        doorL.Rotate(0, 0, target);
+        doorR.Rotate(0, 0, -target);
+        this.Invoke(() => {
+            doorL.Rotate(0, 0, -target);
+            doorR.Rotate(0, 0, target);
+        }, 1f);
+        */
+        newAngleL = targetAngleL;
+        newAngleR = targetAngleR;
+        this.Invoke(() => {
+            newAngleL = startAngle;
+            newAngleR = startAngle;
+        }, 1f);
     }
 
     private void OnDrawGizmos()
